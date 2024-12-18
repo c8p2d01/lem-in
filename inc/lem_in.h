5852dbd44f17364024b99725a658b6f802f9bf28
c8p2d01 <cdahlhof@42wolfsbug.de>
2024-12-18 11:44:41 +0100

    a fresh start

--- inc/lem_in.h
+++ inc/lem_in.h
@@ -12,42 +12,4 @@
 #  define READ_INPUT STDIN_FILENO
 # endif
 
-typedef struct globe {
-	struct s_room	**graph;
-	struct s_room	*start;
-	struct s_room	*end;
-	struct s_link	**linkedlist;
-	size_t			nAnts;
-	size_t			maxLevel;
-	struct s_path	**paths;
-}	globe;
-
-typedef struct s_path {
-	struct	s_room **path;
-	size_t	len;
-	int		ant;
-}	t_path;
-
-void	read_data(globe *data);
-
-void	remove_deadend(globe *data);
-
-void	first_level(globe *data);
-
-bool	first_flow(t_room *node, globe *data);
-
-void	leveling(globe *data);
-
-bool 	after_flow(t_room *node, globe *data);
-
-t_path	*river(t_link *flow, t_room *spring, t_room *estuary);
-
-t_path	**cartograph(globe *data);
-
-void	path_sort(globe *data);
-
-void	ant_march(globe *data);
-
-void	free_globe(globe *data);
-
 #endif
